0a306f42b792342b3f1d21e48526f1bc091d1503
Peter Buczkowski <psibu@me.com>
2020-08-31 16:08:34 +0200

    ESP transfer complete

--- echo_echo/echo_echo.ino
+++ echo_echo/echo_echo.ino
@@ -1,209 +1,74 @@
-#include <Servo.h>
 #include <CapacitiveSensor.h>
+#include <Servo.h>
 
-//ignoring the whole servo class for now
+#define IN D4
+#define OUT D2
+CapacitiveSensor cs_4_2 = CapacitiveSensor(IN,OUT);
 
-class MyServo{
-  private: 
-    Servo servo;
-    byte pin;
-    byte position = 0;
-    byte state = 0;
-    byte increment = 1;
-    byte updateInterval;
-    unsigned long lastUpdate = 0; 
-    byte sweepState = 0;
-   
-    bool finished;
-    bool hasStarted;
-    bool moving = false;
-    
-  public:
-    MyServo(byte pin, byte position){
-      this->pin = pin;
-      this->position = position;
-      finished = false;
-      hasStarted = false;
-    }
-    
-  void init(){
-    servo.attach(pin);
-    servo.write(position);
-  }
-  void detatch(){
-    servo.detach();
-  }
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
-  byte getState(){
-     if (position == 90){
-      state = 1;
-    }else if(position == 0){
-      state = 0;
-    }
-    return state;
-  }
-  byte getPosition(){
-    return position;
-  }
-  
-  void sweep(){
-      for (position = 0; position <= 180; position += increment) { 
-      servo.write(position);              
-      delay(15); 
-    }  
-    for (position = 180; position >= 0; position -= increment) { 
-      servo.write(position);              
-      delay(15); 
-    } 
-
-    
-
-    /*
-      if(position >= 90){
-        hasStarted = true;
-        Serial.println("The Motor is moving back towards 0");
-        increment = -increment;
-      }*/
-   } 
-
-   bool isFinished(){
-     if(hasStarted && position <= 0){
-     finished = true;
-    }
-     return finished;
-   }
-
-   void reset(){
-     finished = false;
-     hasStarted = false;
-     position = 0;
-     increment = -increment;
-     Serial.println("Reset has happened my position is" + position);
-   }
-};  
-
-class RecordButton{
-  private: 
-    byte pin;
-    byte state;
-  public:
-    RecordButton(byte pin){
-      this->pin = pin;
-      state = 0;
-      }  
-
-  void init() {
-    pinMode(pin, INPUT_PULLUP);
-    update();
-    }
-  void update(){
-    if (digitalRead(pin) == LOW){
-        state = 1;
-    }else if(digitalRead(pin) == HIGH){
-        state = 0;
-    }
-  }
-  byte getState(){
-    update();
-    return state;
-  }
-};
-
-RecordButton b1(2);
-//MyServo s1(9,0);
-
-bool moveFirstServo = false;
-
-Servo noClassServoOne;
-Servo noClassServoTwo;
-Servo noClassServoThree;
-int pos = 90;  
+int pos = 90;
 byte increment = 8;
-
-CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2);
-
+bool servosDetached;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); 
   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);
-  //s1.init();
-  b1.init();
-  noClassServoOne.attach(9);
-  noClassServoTwo.attach(10);
-  noClassServoThree.attach(11);
+  servo1.attach(D5);
+  servo2.attach(D6);
+  servo3.attach(D7);
+  servosDetached = false;
 }
 
-void loop() {  
-  
-long start = millis();
-long total1 = cs_4_2.capacitiveSensor(30);
-Serial.println(total1);
-
-if(total1 >= 1900){
-  noClassServoOne.attach(9);
-  noClassServoTwo.attach(10);
-  noClassServoThree.attach(11);
-  sweepServo(noClassServoOne);
-  delay(2000);
-  sweepServo(noClassServoOne);
-  delay(500);
-  sweepServo(noClassServoTwo);
-  delay(2000); 
-  sweepServo(noClassServoTwo);
-  delay(500);
-  sweepServo(noClassServoThree);
-  delay(2000); 
-  sweepServo(noClassServoThree);
-}else{
-  noClassServoOne.detach();
-  noClassServoTwo.detach();
-  noClassServoThree.detach();
-}
-/*
-  if (b1.getState() == 1){  
-    noClassServoOne.attach(9);
-    noClassServoTwo.attach(10);
-    noClassServoThree.attach(11);
-    sweepServo(noClassServoOne);
-    delay(2000);
-    sweepServo(noClassServoOne);
-    delay(500);
-    sweepServo(noClassServoTwo);
-    delay(2000); 
-    sweepServo(noClassServoTwo);
-    delay(500);
-    sweepServo(noClassServoThree);
-    delay(2000); 
-    sweepServo(noClassServoThree);
-  /*if(moveFirstServo == true){
-    s1.sweep();
-    /*if(s1.isFinished()){
-      s1.reset();
-      moveFirstServo = false;
-     // delay(1000);
+void loop() {
+  long total1 = cs_4_2.capacitiveSensor(30);
+   Serial.println(total1);
+   Serial.println(servosDetached);
+   
+    if(total1 > 1000) {
+      Serial.println("Cap Button Pressed");
       
+      if( servosDetached == true){ 
+        servo1.attach(D5);
+        servo2.attach(D6);
+        servo3 .attach(D7);
+        servosDetached = false;
+      }
+
+      servoSession(2000,500);
+
+  }else{
+    servo1.detach();
+    servo2.detach();
+    servo3.detach();
+    servosDetached = true;
   }
-    
-}
-}else{
-   noClassServoOne.detach();
-  noClassServoTwo.detach();
-  noClassServoThree.detach();
-}
-*/
 }
 
-void sweepServo (Servo s){
-
- for(pos = 90; pos >= 0; pos -= increment)  
-  {                                 
-    s.write(pos);        
-    delay(15);                      
-  } 
-  for(pos = 0; pos <=90; pos += increment)   
-  {                                
-    s.write(pos);              
-    delay(15);                     
-  } 
-
+void sweepServo(Servo s){
+  for(pos = 90; pos >= 0; pos -= increment){
+   s.write(pos);
+   delay(15); 
+  }
+  for(pos = 0; pos <= 90; pos += increment){
+    s.write(pos);
+    delay(15);
+  }
 }
+
+void servoSession(int longPause, int shortPause){
+  Serial.println("Servosession reached");
+  sweepServo(servo1);
+  delay(longPause);
+  sweepServo(servo1);
+  delay(shortPause);
+  sweepServo(servo2);
+  delay(longPause);
+  sweepServo(servo2);
+  delay(shortPause);
+  sweepServo(servo3);
+  delay(longPause);
+  sweepServo(servo3);  
+  }
