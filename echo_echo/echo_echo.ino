8c26d80dd21ac3c818fcbc3fd7cff205ea5e32a8
Peter Buczkowski <psibu@me.com>
2020-12-09 16:29:42 +0100

    update: states_stable

--- echo_echo/echo_echo.ino
+++ echo_echo/echo_echo.ino
@@ -1,6 +1,56 @@
 #include <CapacitiveSensor.h>
 #include <Servo.h>
 
+/*class myServo{
+  Servo servo;
+  int pos;
+  int startPos = 0;
+  int endPos;
+  int updateInterval;
+  int delay;
+  int servoSpeed; 
+  bool movingForward;
+  unsigned long lastUpdate;
+ 
+
+
+
+
+  public:
+    myServo(int interval){
+      updateInterval = interval;
+      servoSpeed = 5;
+    }
+
+    //constructor to sweep in a correct delay
+    myServo(int _delay, int _updateInterval, int _servoSpeed){
+      updateInterval = _updateInterval;
+      delay = _delay;
+      servoSpeed = _servoSpeed;
+    }
+
+    void Attach(int pin){
+      servo.attach(pin);
+      servo.write(startPos);
+    }
+    void Detatch(){
+      servo.detach();
+    }
+    void Update(){
+      if(millis() - lastUpdate + delay > delay){
+       // lastUpdate = millis();
+        pos += servoSpeed;
+        servo.write(pos);
+        //Serial.prin
+        if((pos >= endPos && pos <= startPos && millis() - lastUpdate + delay > delay) ){
+          lastUpdate = millis();
+          pos -= servoSpeed; 
+        }
+      }
+    }
+};
+*/
+
 #define IN 2 //GPIO 2 But Pin 4
 #define OUT 4//GPIO 4 But Pin 2
  
@@ -9,24 +59,35 @@ CapacitiveSensor cs_4_2 = CapacitiveSensor(IN, OUT);
 /* 
   state 0 = serial control of all 3 servos
   state 1 =  session
+  state 2 = all together
   state 3 = random session
 */
-byte state = 1;
+byte state = 2;
+int parsedValue = 0;
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
+int capThreshold = 3500;
+int longPause = 6000;
+int shortPause = 3000;
 
 int pos = 0;
 int oldPos = 0;
-int startPos = 0;
-int endPos = 180;
-int servoSpeed = 2;
-bool servosDetached;
-int parsedValue = 0;
+int startPos = 90;
+int endPos = 120;
 
-int capThreshold = 4000;
+int servoSpeed = 1;
+int updateInterval = 15;
+bool servosDetached;
+bool movingForward = false;
+
+bool capPressed = false;
+
+
+//unsigned long previousMillis = 0;
+//long interval = 1000;
 
 
 void setup(){
@@ -42,8 +103,8 @@ void setup(){
 }
 
 void loop(){
+  //long currentMillis = millis();
   long total1;
-
   switch(state){
     case 0:
       if(Serial.available() > 0){
@@ -55,7 +116,7 @@ void loop(){
             servo1.write(pos); 
             servo2.write(pos); 
             servo3.write(pos); 
-            delay(15);
+            delay(updateInterval);
             oldPos = pos;
           }
         }else if(parsedValue > startPos && oldPos <= pos){
@@ -63,32 +124,34 @@ void loop(){
             servo1.write(pos); 
             servo2.write(pos); 
             servo3.write(pos); 
-            delay(15);
+            delay(updateInterval);
             oldPos = pos;
           }
         } 
       }    
     break; 
-
     case 1: 
       total1 = cs_4_2.capacitiveSensor(30);
-      
       Serial.println(total1);
-      if (Serial.available() > 0 && total1 > capThreshold){
-      Serial.println("Cap Button Pressed");
-        if (servosDetached == true){
-          servo1.attach(14); //GPIO 14 But D5
-          servo2.attach(12); //GPIO 12 But D6
-          servo3.attach(13); //GPIO 13 But D7
-          servosDetached = false;
-        }
-        //servoSession(2000,500);
-        sweepAll();
-      }else{
-       /* servo1.detach();
-        servo2.detach();
-        servo3.detach();
-        servosDetached = true;*/
+      if (total1 > capThreshold && capPressed == false){
+        capPressed = true;
+        Serial.println("Cap Button Pressed");
+      }
+      if(capPressed == true){
+        servoSession(longPause);
+        capPressed = false;
+      }
+    break;
+    case 2:
+    total1 = cs_4_2.capacitiveSensor(30);
+    Serial.println(total1);
+    if (total1 > capThreshold && capPressed == false){
+        capPressed = true;
+        Serial.println("Cap Button Pressed");
+      }
+      if(capPressed == true){
+        sweepAll(100);
+        capPressed = false;
       }
     break;
     default:
@@ -97,50 +160,67 @@ void loop(){
       servo3.write(0);
   }
 }
-        
-  
-  /*for(pos = 0; pos <= 180; pos +=1){
-    servo1.write(pos);
-    delay(15);
-  }
-  for(pos = 180; pos >= 0; pos -=1){
-      servo1.write(pos);
-      delay(15);
-  }
-*/
-
 
 //Function to sweep a Servo from Start to Endposition and Back
 void sweepServo(Servo s){
   for (pos = startPos; pos <= endPos; pos += servoSpeed){
     s.write(pos);
-    delay(15);
+    delay(updateInterval);
   }
   for (pos = endPos; pos >= startPos; pos -= servoSpeed){
     s.write(pos);
-    delay(15);
+    delay(updateInterval);
   }
 }
 
+void pressButton(Servo s){
+  if(!movingForward){
+    for (pos = startPos; pos <= endPos; pos += servoSpeed){
+      s.write(pos);
+      delay(updateInterval);
+    }
+  }else if(movingForward){
+    for (pos = endPos; pos >= startPos; pos -= servoSpeed){
+    s.write(pos);
+    delay(updateInterval);
+    } 
+  }
+}
+
+void pressMultiButtons(){
+  if(!movingForward){
+    for (pos = startPos; pos <= endPos; pos += servoSpeed){
+        servo1.write(pos);
+        servo2.write(pos);
+        servo3.write(pos);
+        delay(updateInterval);
+    }
+  }else if(movingForward){
+    for (pos = endPos; pos >= startPos; pos -= servoSpeed){
+      servo1.write(pos);
+      servo2.write(pos);
+      servo3.write(pos);
+      delay(updateInterval);
+    }
+  }
+}
+ 
+void servoSession(int longPause){
+  pressButton(servo1);
+  delay(longPause);
+  pressButton(servo2);
+  delay(longPause);
+  pressButton(servo3);
+  movingForward = !movingForward;
+}
+
+void sweepAll(int longPause){
+  pressMultiButtons();
+  movingForward = !movingForward;
+}
 void sweepAll(){
   sweepServo(servo1);
   sweepServo(servo2);
   sweepServo(servo3);
   delay(2000);
 }
-
-void servoSession(int longPause, int shortPause){
-  Serial.println("Servosession reached");
-  sweepServo(servo1);
-  //delay(longPause);
-  sweepServo(servo1);
-  delay(shortPause);
-  sweepServo(servo2);
-  delay(longPause);
-  sweepServo(servo2);
-  delay(shortPause);
-  sweepServo(servo3);
-  delay(longPause);
-  sweepServo(servo3);
-}
-
